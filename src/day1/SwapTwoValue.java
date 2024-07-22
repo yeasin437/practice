6d49edde1e8101cbdd1448878dc06cd29a1a9c9e
Yeasin <career3517@gmail.com>
2024-07-22 16:51:32 -0400

    first commit

---
+++ src/day1/SwapTwoValue.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class SwapTwoValue {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		a = a +b ;
+		b = a - b;
+		
+		a = a - b;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+
+	}
+
+}
